f2ba7ec4a430fef20cc9b923e9e3afce7aad7e41
xansec <76011430+xansec@users.noreply.github.com>
2022-09-22 01:34:34 -0400

    Resolve more compiler complaints

--- sbor/fuzz/fuzz_targets/roundtrip.rs
+++ sbor/fuzz/fuzz_targets/roundtrip.rs
@@ -1,6 +1,6 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
-use sbor::{decode_any, encode_any, Encoder};
+use sbor::{decode_any, encode_any};
 
 fuzz_target!(|data: &[u8]| {
     let _ = fuzz(data);
@@ -11,7 +11,7 @@ fn fuzz(data: &[u8]) -> Result<(), ()> {
 
     //let mut bytes = Vec::new();
     //let mut encoder = Encoder::with_type_id(&mut bytes);
-    encode_any(&value);
+    let mut bytes = encode_any(&value);
 
     let value2 = decode_any(&bytes).expect("encoded bytes to deserialize");
     assert_eq!(value, value2, "roundtrip failure");
